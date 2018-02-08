7e2340def1b3eba47de8532b9d572a30f0f5dd9b
Remco Kortekaas <remcokortekaas@Remcos-MacBook-Pro.local>
2018-02-08 14:26:07 +0100

    files added to repo

---
+++ src/main/java/com/remcokortekaas/app/SendMessages.java
@@ -0,0 +1,13 @@
+package com.remcokortekaas.app;
+
+public interface SendMessages {
+
+	public void sendAdd(Client client, boolean isLeader);
+
+	public void sendUpdate(Client client, boolean isLeader);
+
+	public void sendDelete(Integer accountNumber, boolean isLeader);
+
+	public void sendCreateBank (ClientDB clientDB, boolean isLeader);
+
+}
